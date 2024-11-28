5cd619a513250fe9505553c29754653ca08c5aa9
JordanSanchez06 <jordansanchez100603@gmail.com>
2024-11-28 00:00:04 -0500

    added our domain as an origin

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://summarizer4331.jordanshouse.site');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
+    res.setHeader('Access-Control-Allow-Origin', 'https://summarizer4331.jordanshouse.site');
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, PATCH, DELETE, OPTIONS'
