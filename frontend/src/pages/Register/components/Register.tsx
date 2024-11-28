89493e4f18672a9265e00b223ec9d7d262829218
JordanSanchez06 <jordansanchez100603@gmail.com>
2024-11-28 01:08:52 -0500

    added our domain as an origin

--- frontend/src/pages/Register/components/Register.tsx
+++ frontend/src/pages/Register/components/Register.tsx
@@ -95,7 +95,7 @@ function Register() {
         var obj = {fullName: fullName, login: loginName, password: loginPassword };
         var js = JSON.stringify(obj);
         try {
-            const response = await fetch('https://summarizer4331.jordanshouse.site/api/login',
+            const response = await fetch('https://summarizer4331.jordanshouse.site/api/signup',
                 {
                     method: 'POST', body: js, headers: {
                         'Content-Type':
