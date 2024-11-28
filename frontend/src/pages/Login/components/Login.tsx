4afcbbd040211e640e2e86a685384678023fce25
JordanSanchez06 <jordansanchez100603@gmail.com>
2024-11-28 00:49:02 -0500

    added our domain as an origin

--- frontend/src/pages/Login/components/Login.tsx
+++ frontend/src/pages/Login/components/Login.tsx
@@ -62,7 +62,7 @@ function Login() {
         var obj = { login: loginName, password: loginPassword };
         var js = JSON.stringify(obj);
         try {
-            const response = await fetch('http://localhost:5000/api/login',
+            const response = await fetch('http://3.238.62.241:5000/api/login',
                 {
                     method: 'POST', body: js, headers: {
                         'Content-Type':
