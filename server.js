44ceaff4beee69c1823ffaaad93eb20779467c4b
JordanSanchez06 <jordansanchez100603@gmail.com>
2024-11-28 00:17:23 -0500

    added our domain as an origin

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(cors({
-    origin: 'https://summarizer4331.jordanshouse.site',  // Allow this origin
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],  // Allowed methods
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],  // Allowed headers
-}));
-app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://summarizer4331.jordanshouse.site');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.status(200).end(); // Respond with a successful status for OPTIONS requests
-});
+app.use(cors());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
 app.listen(5000); // start Node + Express server on port 5000
 
 const MongoClient = require('mongodb').MongoClient;
