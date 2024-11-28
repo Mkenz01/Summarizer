335b8bb0e91f8e7da7b5a6b1906d2911c6ce08d4
JordanSanchez06 <jordansanchez100603@gmail.com>
2024-11-28 00:29:58 -0500

    added our domain as an origin

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(cors({
-    origin: 'https://summarizer4331.jordanshouse.site',  // Allow only this origin
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],  // Allowed methods
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],  // Allowed headers
-    preflightContinue: false, // Automatically end the OPTIONS requests
-    optionsSuccessStatus: 200, // For legacy browser support (status code 200 instead of 204)
-}));
+app.use(cors());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
 app.listen(5000); // start Node + Express server on port 5000
 
 const MongoClient = require('mongodb').MongoClient;
