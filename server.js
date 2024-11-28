c4c2264e93c08f7d29acb0a42332402fdc1f742f
JordanSanchez06 <jordansanchez100603@gmail.com>
2024-11-28 00:21:15 -0500

    added our domain as an origin

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use((req, res, next) => {
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.setHeader(
+        'Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT'
+    )
+    res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, PATCH, DELETE, OPTIONS'
     );
