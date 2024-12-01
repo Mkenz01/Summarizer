f011020300cd219fe4449c2136880875e364a90f
samww <jordansanchez100603@gmail.com>
2024-12-01 12:56:59 -0500

    file upload working

--- frontend/src/pages/Home/components/Summary.tsx
+++ frontend/src/pages/Home/components/Summary.tsx
@@ -2,41 +2,13 @@ import React, { useState } from 'react';
 
 
 function Summary() {
-    /*const _ud: any = localStorage.getItem('user_data');
-    const ud = JSON.parse(_ud);
-    /*const userId = ud.userId;*/
-    const questionSeparator = ";;;";
-    const answerSepartator =  ":::";
-    const openAnswer = "{"
-    const closeAnswer = "}";
-    var quiz = "question 1 ::: {answer 1} ::: answer 2 ::: answer 3 ;;; question 2 ::: answer 1 ::: {answer 2} ::: answer 3 ;;;question 3 ::: answer 1 ::: answer 2 ::: {answer 3}";
-    /*async function addCard(e: any): Promise<void> {
-        e.preventDefault();
-        let obj = { userId: userId};
-        let js = JSON.stringify(obj);
-        try {
-            const response = await
-                fetch('https://summarizer4331.jordanshouse.site/api/getQuiz',
-                    {
-                        method: 'Get', body: js, headers: {
-                            'Content-Type':
-                                'application/json'
-                        }
-                    });
-            let txt = await response.text();
-            let res = JSON.parse(txt);
-            quiz = res.text;
-        }
-        catch (error: any) {
-            console.log(error)
-        }
-    }*/
+ const summary = "summary"
 
 
 
     return (
         <div>
-            `${quiz}`
+            `${summary}`
         </div>
     );
 }
