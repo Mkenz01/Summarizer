76b2b2b42bbbf4215840eb55f902ebfd780255fa
samww <jordansanchez100603@gmail.com>
2024-12-05 01:07:00 -0500

    commmen t out relaod

--- summarization.js
+++ summarization.js
@@ -8,10 +8,9 @@ const fs = require('fs');
 //const { pipeline } = require('@huggingface/transformers');
 const { OpenAI } = require('openai');
 //const { SummarizationPipeline } = require('@huggingface/transformers');
-const {openAiAPI} = require('./.env');
 
 const openai = new OpenAI({
-  apiKey: "process.env.OPENAI_API_KEY", dangerouslyAllowBrowser: true,
+  apiKey: process.env.OPENAI_API_KEY, dangerouslyAllowBrowser: true,
 });
 
 async function extractTextFromFile(filePath) {
